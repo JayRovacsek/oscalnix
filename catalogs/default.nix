4010f123a55acb344b5d0089c01d3ce2b7264a67
jayrovacsek <jay@rovacsek.com>
2025-06-10 22:03:14 +1000

    feat: add catalogs and hash references

---
+++ catalogs/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, self, ... }:
+let
+  lib = self.lib pkgs;
+in
+{
+  au-ism-catalog = lib.catalog {
+    name = "Australian Federal Information Security Manual";
+    source = self.sources.${pkgs.system}.au-ism-v2025-03-31;
+    path = "./ISM_catalog.json";
+    meta = {
+      version = "2025.03.31";
+      oscal-version = "1.1.2";
+    };
+  };
+}
